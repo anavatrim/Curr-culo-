03d9b03316608a555eb9c22b3b010a5ae7f3c583
Vatrim <anavatrim@hotmail.com>
2025-07-07 17:37:36 -0300

    .

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the current directory
 app.use(express.static('.'));
 
-// For all other routes, serve the index.html file
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
